18db7b40b517c347af6b7740dbd90ea82fb34089
mlbelobraydi <mlbelobraydi@gmail.com>
2020-08-05 09:42:32 -0600

    Update dbf900_main.py
    Moved the file decoding and splitting by the block_size to this .py instead of needing it in the working file. This alleviates the need to copy a large decoding section into each subsequent working file.

--- dbf900_main.py
+++ dbf900_main.py
@@ -8,7 +8,24 @@ Created on Tue Aug  4 15:41:40 2020
 import codecs
 from dbf900_formats import pic_yyyymmdd, pic_yyyymm, pic_latlong, pic_coord, pic_numeric, pic_any
 
-##potential add to decode file and break into the individual records 
+def decode_file(file, block_size): ##Requires string for the file location and integer for blocksize
+    print('opening',file)
+    with open(file, 'rb') as ebcdicfl: #Reads the .ebc file
+        data = ebcdicfl.read()
+    
+    print('decoding...')
+    ascii_txt = codecs.decode(data, 'cp1140') #decodes the entire .ebc file to ascii
+    ##This decoding method still leaves a few uncoded "/x0" type characters
+    
+    split_records = [] ##empty array for records
+    
+    print('separating records...')
+    for index in range(0, len(ascii_txt), block_size): ##Creates an array for all records in the file
+        split_records.append(ascii_txt[index : index + block_size])    
+    
+    print('returning records...')
+    return split_records
+
 
 def parse_record(record, layout):
     values = dict()
