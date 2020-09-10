11fbc63318463a64340047bf3018faa999274feb
mlbelobraydi <mlbelobraydi@gmail.com>
2020-09-09 20:18:45 -0600

    Update dbf900_formats_bytes.py
    created ebc_decode function to remove the process from each function below and to make it callable from other parts of the program. Also worked with pic_signed function to handle the positive longitude values from TXRRC
    
    Co-Authored-By: skylerbast <30266974+skylerbast@users.noreply.github.com>

--- dbf900_formats_bytes.py
+++ dbf900_formats_bytes.py
@@ -11,9 +11,17 @@ from datetime import datetime
 from datetime import date
 from array import array
 
+##From https://github.com/skylerbast/TxRRC_data
+##converts bytes to string through codecs decoding
+## Should be added to dbf900_main_bytes when it is working
+def ebc_decode(data):
+    ebcdic_decoder = codecs.getdecoder('cp1140')
+    decoded = ebcdic_decoder(data)
+    val = decoded[0]
+    return val
 
 def pic_yyyymmdd(date):
-    date = codecs.decode(date, 'cp1140')
+    date = ebc_decode(date)
     #Changes format YYYYMMDD from a series of numbers to datetime object
     try:
         val = datetime.strptime(date, '%Y%m%d').strftime('%m/%d/%Y')
@@ -22,7 +30,7 @@ def pic_yyyymmdd(date):
     return val
     
 def pic_yyyymm(yyyymm):
-    yyyymm = codecs.decode(yyyymm, 'cp1140')
+    yyyymm = ebc_decode(yyyymm)
     #Changes format YYYYMM from a series of numbers to datetime object
     #makes the date the first day of the month
     try:
@@ -33,7 +41,7 @@ def pic_yyyymm(yyyymm):
     return val
 
 def pic_numeric(num):
-    num = codecs.decode(num, 'cp1140')
+    num = ebc_decode(num)
     try: ##using a try to ensure the values passed are actually 0-9 with no other characters
         val = int(num)
     except:
@@ -42,7 +50,7 @@ def pic_numeric(num):
     return val
 
 def pic_any(string): #need to confirm the numberof characters
-    string  = codecs.decode(string, 'cp1140')
+    string  = ebc_decode(string)
     STRIP_PIC_X = True # Set this to False if trimming PIC X causes problems.
     val = str(string)
     if STRIP_PIC_X == True:
@@ -50,24 +58,25 @@ def pic_any(string): #need to confirm the numberof characters
 
     return val
 
-def pic_signed(signed, decimal=0): #replacement for pic_latlong and pic_coord
+def pic_signed(signed,name,decimal=0): #replacement for pic_latlong and pic_coord
     # Converts an EBCDIC Signed number to Python float
     # 'signed' must be EBCDIC-encoded raw bytes -- this will not work
     # if the data has been converted to ASCII.
-    print('original sent bytes:', signed)
+    ## info here http://www.3480-3590-data-conversion.com/article-signed-fields.html
     signed_raw = array('B', signed);
     val = float(0);
-    print('converted to array:', signed_raw)
     
     # Bytes 1 to n-1 are stored as plain EBCDIC encoded digits
     for i in signed_raw:
         val = val * 10 + (i & 0x0F)
     
-    print('result by adding array:',val)
-    
+   
     # If the penultimate nibble == 0xD, then the number is negative. Otherwise,
     # it is either positive or unsigned.
     val = (val * (-1 if signed_raw[-1] >> 4 == 0xD else 1)) / 10**decimal
-    print('final val to be sent back:',val)
+    
+    ##TXRRC signs longitude as a positive value with 0xC and requires transformation to a negative number
+    if 'LONGITUDE' in name and val > 0: ##This is only appropriate for western hemisphere
+        val = -val
     
     return val
