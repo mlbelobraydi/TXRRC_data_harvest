c602f5900d84fabf9de8dd8fd2c0417464b594d5
mlbelobraydi <mlbelobraydi@gmail.com>
2020-09-28 11:22:20 -0600

    Update WorkingFileForTesting.py
    updated to fit the new naming convention of the layout files

--- WorkingFileForTesting.py
+++ WorkingFileForTesting.py
@@ -9,7 +9,7 @@ import sys
 import pandas as pd
 import json
 from dbf900_main_bytes import yield_blocks, parse_record
-from dbf900_layouts_bytes import dbf900_layout
+from layouts_wells_dbf900 import dbf900_layout
 from dbf900_formats_bytes import pic_yyyymmdd, pic_numeric, pic_any
 
 
